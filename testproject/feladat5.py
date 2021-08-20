df014badf839188a9b4bd8835e39daa1eaa665f5
krfprogit <krfprogit@gmail.com>
2021-08-21 00:50:31 +0200

    oldal betölve

--- testproject/feladat5.py
+++ testproject/feladat5.py
@@ -1,3 +1,17 @@
+from selenium import webdriver
+import time
+
+PATH = "C:\\Windows\\chromedriver.exe"
+browser = webdriver.Chrome(PATH)
+
+URL = "https://witty-hill-0acfceb03.azurestaticapps.net/periodic_table.html"
+browser.get(URL)
+browser.maximize_window()
+
+# time.sleep(2)
+# browser.quit()
+
+
 with open('data.txt', 'r', encoding="UTF-8") as f:
     txt_lines = f.readlines()
     print(type(txt_lines))

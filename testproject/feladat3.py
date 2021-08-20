0f3a5adda4762527a8d8204b23e785a5036ed52f
krfprogit <krfprogit@gmail.com>
2021-08-20 20:18:55 +0200

    TC01: leggyorsabb keresés!

--- testproject/feladat3.py
+++ testproject/feladat3.py
@@ -12,25 +12,36 @@ browser.maximize_window()
 # input
 szam_min = 1
 szam_max = 100
-lepesek_szama = 0
+szam_tipp = (szam_min + szam_max) // 2
 szam_megtalalt = 0
+lepesek_szama = 0
 
 szam_input = browser.find_element_by_xpath('/html/body/div/div[2]/input')
 bevitel_gomb = browser.find_element_by_xpath('/html/body/div/div[2]/span/button')
-valasz_uzenet = browser.find_element_by_xpath('/html/body/div/p[5]')
+valasz_uzenet_1 = browser.find_element_by_xpath('/html/body/div/p[5]')
+valasz_uzenet_2 = browser.find_element_by_xpath('/html/body/div/p[3]')
 szamlalo_app = browser.find_element_by_xpath('/html/body/div/div[3]/p/span')
 restart_gomb = browser.find_element_by_xpath('/html/body/div/div[1]/div[2]/button')
 
-for i in range(szam_min, szam_max + 1):
+while True:
     lepesek_szama += 1
     szam_input.clear()
-    szam_input.send_keys(i)
+    szam_input.send_keys(szam_tipp)
     bevitel_gomb.click()
-    if valasz_uzenet.text == 'Yes! That is it.':
-        szam_megtalalt = i
+    if valasz_uzenet_1.text == 'Yes! That is it.':
+        szam_megtalalt = szam_tipp
         break
+    elif valasz_uzenet_2.text == 'Guess lower.':
+        szam_max = szam_tipp
+        szam_tipp = (szam_min + szam_max) // 2
+    else:
+        szam_min = szam_tipp
+        szam_tipp = (szam_min + szam_max) // 2
+
 print('szám: ', szam_megtalalt)
 print("lépések: ", lepesek_szama)
+
+assert valasz_uzenet_1.text == 'Yes! That is it.'
 
 # time.sleep(2)
 # browser.quit()

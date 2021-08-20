7efc77ebc8b52a1f86499fbe5d73c5d8f89a8beb
krfprogit <krfprogit@gmail.com>
2021-08-20 18:43:29 +0200

    TC01 nem ok

--- testproject/feladat2.py
+++ testproject/feladat2.py
@@ -8,5 +8,15 @@ URL = "https://witty-hill-0acfceb03.azurestaticapps.net/film_register.html"
 browser.get(URL)
 browser.maximize_window()
 
-time.sleep(2)
-browser.quit()
+# TC01
+filmek_input = browser.find_elements_by_xpath('//div[@class="container-total"]')
+for f in filmek_input:
+    print(f)
+print(len(filmek_input))
+
+film_input = browser.find_element_by_xpath('//div[@class="container-total"]/a[1]/div/img')
+# film_input = browser.find_element_by_xpath('/html/body/div[2]/div[3]/a[1]')
+print(len(film_input))
+
+# time.sleep(2)
+# browser.quit()
